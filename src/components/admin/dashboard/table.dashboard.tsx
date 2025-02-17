c18ced6ad87f3a2c1807b5f3fc9758b2ce9cc543
lbachannel <anlb19749@gmail.com>
2025-02-17 21:56:18 +0700

    multiple filter activity log

--- src/components/admin/dashboard/table.dashboard.tsx
+++ src/components/admin/dashboard/table.dashboard.tsx
@@ -1,9 +1,31 @@
 import { getAllActivityLogAPI } from "@/services/api";
-import { ProColumns, ProTable } from "@ant-design/pro-components";
+import { ActionType, ProColumns, ProTable } from "@ant-design/pro-components";
 import dayjs from "dayjs";
-import { useState } from "react";
-
+import { useRef, useState } from "react";
+import 'styles/dashboard.scss';
+type TSearch = {
+    activityGroup: string;
+    activityType: string[];
+}
 const TableDashBoard = () => {
+
+    const [activityGroup, setActivityGroup] = useState<string>('');
+    const [activityType, setActivityType] = useState<string[]>([]);
+
+    const handleActivityGroupChange = (value: string) => {
+        setActivityGroup(value);
+        setActivityType([]);
+    };
+
+    const handleActivityTypeChange = (value: string[]) => {
+        setActivityType(value);
+        
+    };
+
+    const handleReset = () => {
+        setActivityGroup("");
+        setActivityType([]);
+    };
 
     const [meta, setMeta] = useState({
         current: 1,
@@ -11,12 +33,14 @@ const TableDashBoard = () => {
         pages: 0,
         total: 0
     })
+
+    const actionRef = useRef<ActionType>();
     
     // table: activity log
     const columns: ProColumns<IActivityLogTable>[] = [
         {
-            title: "Active group",
-            dataIndex: "",
+            title: "Activity group",
+            dataIndex: "activityGroup",
             valueType: 'select',
             ellipsis: true,
             valueEnum: {
@@ -24,9 +48,10 @@ const TableDashBoard = () => {
                 user: { text: 'User' }
             },
             fieldProps: {
-                style: { marginLeft: 10 } // Tạo khoảng cách giữa label và input
+                style: { marginLeft: 10 },
+                onChange: handleActivityGroupChange
             },
-            hidden: true
+            hidden: true,
         },
         {
             title: "Modified time",
@@ -47,17 +72,22 @@ const TableDashBoard = () => {
             hideInSearch: true
         },
         {
-            title: "Active type",
+            title: "Activity type",
             dataIndex: "activityType",
             valueType: 'select',
             ellipsis: true,
-            valueEnum: {
-                book: { text: 'Create book' },
-                user: { text: 'Create user' }
-            }, 
+            key: activityGroup,
             fieldProps: {
-                style: { marginLeft: 10 }
-            }
+                key: activityGroup,
+                style: { marginLeft: 10 },
+                onChange: handleActivityTypeChange,
+                mode: "multiple",
+            },
+            valueEnum: activityGroup === 'book'
+            ? { 'Create book': { text: 'Create book' }, 'Update book': { text: 'Update book' }, 'Delete book': { text: 'Delete book' } }
+            : activityGroup === 'user'
+                ? { 'Create user': { text: 'Create user' }, 'Update user': { text: 'Update user' }, 'Delete user': { text: 'Delete user' } }
+                : {},
         },
         {
             title: "Description",
@@ -101,15 +131,35 @@ const TableDashBoard = () => {
     ]
     return (
         <>
-            <ProTable<IActivityLogTable>
+            <ProTable<IActivityLogTable, TSearch>
                 columns={columns}
                 cardBordered
-                rowKey="id"
+                onReset={handleReset}
+                actionRef={actionRef}
                 request={async (params) => {
                     let query = "";
                     if (params) {
                         query += `page=${(params?.current ?? 1) - 1}&size=${params?.pageSize ?? 5}`;
                         query += '&sort=executionTime,desc';
+                        if (activityGroup) {
+                            if (activityGroup === "book") {
+                                query += `&filter=activityGroup~'${activityGroup}'`;
+                            }
+                            if (activityGroup === "user") {
+                                query += `&filter=activityGroup~'${activityGroup}'`;
+                            }
+                        }
+
+                        if (activityType && activityType.length > 0 ) {
+                            const arrActivityType = Array.isArray(activityType) ? activityType : [activityType];
+                            if (arrActivityType.length > 1) {
+                                const filterValue = arrActivityType.map(type => `'${type}'`).join(", ");
+                                const filterParam = `activityType in [${filterValue}]`;
+                                query += `&filter=${encodeURIComponent(filterParam)}`;
+                            } else {
+                                query += `&filter=activityType~'${arrActivityType[0]}'`;
+                            }
+                        }
                     }
                     const response = await getAllActivityLogAPI(query);
                     if (response.data) {
@@ -122,6 +172,7 @@ const TableDashBoard = () => {
                         total: response.data?.meta.total
                     }
                 }}
+                rowKey="id"
                 pagination={{
                     current: meta.current,
                     pageSize: meta.pageSize,
